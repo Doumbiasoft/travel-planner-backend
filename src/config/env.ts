f4b90068d119ef3567dba451061678739128a26b
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-10 11:28:10 -0400

    updated the env config file with new variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,10 +5,26 @@ interface Env {
   NODE_ENV: string;
   PORT: number;
   API_BASE_URL: string;
+  MONGO_URI: string;
+  SMTP_HOST: string;
+  SMTP_PORT: number;
+  SMTP_TLS: boolean;
+  SMTP_USER: string;
+  SMTP_PASS: string;
+  MAIL_FROM: string;
+  MAIL_FROM_NAME: string;
 }
 
 export const ENV: Env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost",
   PORT: Number(process.env.PORT) || 3000,
+  MONGO_URI: process.env.MONGO_URI || "",
+  SMTP_HOST: process.env.SMTP_HOST || "",
+  SMTP_PORT: Number(process.env.SMTP_PORT) || 0,
+  SMTP_TLS: Boolean(process.env.SMTP_TLS),
+  SMTP_USER: process.env.SMTP_USER || "",
+  SMTP_PASS: process.env.SMTP_PASS || "",
+  MAIL_FROM: process.env.MAIL_FROM || "",
+  MAIL_FROM_NAME: process.env.MAIL_FROM_NAME || "",
 };
